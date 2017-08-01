7096f6a9a9a662c91d306ba0cff8976fd28c4c63
Lisa Hufnagel <lmhufnagel@gmail.com>
2017-08-01 21:31:31 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,8 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     current_line = "The line is currently:"
-    katz_deli.each.with_index(1) do |person, i|
-      current_line << " #{i}. #{person}"
+    katz_deli.each.with_index do |person, i|
+      current_line << " #{i+1}. #{person}"
     end
     puts current_line
   end
